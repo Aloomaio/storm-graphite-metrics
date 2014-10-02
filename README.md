58b88af65a9d2ad29ea8e32fb9911393e7d65a25
yairwein <yairwein@gmail.com>
2014-10-02 09:55:54 +0300

    allow overwriting graphite format

--- README.md
+++ README.md
@@ -5,8 +5,8 @@ Metrics consumer for apache storm that exports metrics into graphite
 In Storm configuration
 ```java
    conf.registerMetricsConsumer(GraphiteMetricsConsumer.class, 1);
-   conf.put(GraphiteMetricsConsumer.GRAPHITE_HOST_KEY, "<your graphite host>"); //default localhost
-   conf.put(GraphiteMetricsConsumer.GRAPHITE_PORT_KEY, "<your graphite port>"); //default 2003
+   conf.put(GraphiteMetricsConsumer.GRAPHITE_HOST, "<your graphite host>"); //default localhost
+   conf.put(GraphiteMetricsConsumer.GRAPHITE_PORT, "<your graphite port>"); //default 2003
 ```
 ##Download
 The jar is distributed via maven central.
--- src/main/java/com/github/aloomaio/storm/metrics/GraphiteMetricsConsumer.java
+++ src/main/java/com/github/aloomaio/storm/metrics/GraphiteMetricsConsumer.java
@@ -40,20 +40,38 @@ import backtype.storm.task.TopologyContext;
 public class GraphiteMetricsConsumer implements IMetricsConsumer {
 	public static final Logger LOG = LoggerFactory.getLogger(GraphiteMetricsConsumer.class);
 
-	public static final String GRAPHITE_HOST_KEY = "alooma.metrics.graphite.host";
-	public static final String GRAPHITE_PORT_KEY = "alooma.metrics.graphite.port";
+	public static final String GRAPHITE_HOST = "alooma.metrics.graphite.host";
+	public static final String GRAPHITE_PORT = "alooma.metrics.graphite.port";
+	public static final String GRAPHITE_OUTPUT_FORMAT = "alooma.metrics.graphite.format";
+	public static final String GRAPHITE_OUTPUT_FORMAT_SINGLE = "alooma.metrics.graphite.format_single";
 
 	/*
 	 * default values for graphite host and port
 	 */
 	private String graphiteHost = "localhost";
 	private int graphitePort = 2003;
+	private String format = String.format("%s.%s.%s.%s.%s.%s", 
+			OutputFormatConstructs.SRC_WORKER_HOST.magicSequence,
+			OutputFormatConstructs.SRC_WORKER_PORT.magicSequence,
+			OutputFormatConstructs.SRC_COMPONENT_ID.magicSequence,
+			OutputFormatConstructs.SRC_TASK_ID.magicSequence,
+			OutputFormatConstructs.METRIC_NAME.magicSequence,
+			OutputFormatConstructs.METRIC_KEY.magicSequence);
+	
+	private String formatSingle = String.format("%s.%s.%s.%s.%s", 
+			OutputFormatConstructs.SRC_WORKER_HOST.magicSequence,
+			OutputFormatConstructs.SRC_WORKER_PORT.magicSequence,
+			OutputFormatConstructs.SRC_COMPONENT_ID.magicSequence,
+			OutputFormatConstructs.SRC_TASK_ID.magicSequence,
+			OutputFormatConstructs.METRIC_NAME.magicSequence);
 	
 	public void prepare(Map stormConf, Object registrationArgument,
 			TopologyContext context, IErrorReporter errorReporter) {
 		LOG.trace("preparing grapite metrics consumer");
-		String graphiteHostFromConf = (String) stormConf.get(GRAPHITE_HOST_KEY);
-		String graphitePortFromConf = (String) stormConf.get(GRAPHITE_PORT_KEY);
+		String graphiteHostFromConf = (String) stormConf.get(GRAPHITE_HOST);
+		String graphitePortFromConf = (String) stormConf.get(GRAPHITE_PORT);
+		String formatFromConf = (String) stormConf.get(GRAPHITE_OUTPUT_FORMAT);
+		String formatSingleFromConf = (String) stormConf.get(GRAPHITE_OUTPUT_FORMAT_SINGLE);
 		if (null != graphiteHostFromConf) {
 			graphiteHost = graphiteHostFromConf;
 		}
@@ -80,10 +98,10 @@ public class GraphiteMetricsConsumer implements IMetricsConsumer {
 					//storm uses raw map without generic types
 					Set<Map.Entry> entries = ((Map) p.value).entrySet();
 					for(Map.Entry e : entries) {
-						graphiteWriter.printf("%s.%s.%s.%s.%s %s %d\n", taskInfo.srcWorkerHost, taskInfo.srcWorkerPort,taskInfo.srcTaskId, p.name, e.getKey(), e.getValue(), taskInfo.timestamp);
+						graphiteWriter.printf("%s %s %d\n", OutputFormatConstructs.formatStringForGraphite(format, taskInfo, p.name, (String) e.getKey()), e.getValue(), taskInfo.timestamp);
 					}
 				} else if(p.value instanceof Number) {
-					graphiteWriter.printf("%s.%s.%s.%s %s %d\n", taskInfo.srcWorkerHost, taskInfo.srcWorkerPort, taskInfo.srcTaskId, p.name, p.value, taskInfo.timestamp);
+					graphiteWriter.printf("%s %s %d\n", OutputFormatConstructs.formatStringForGraphite(formatSingle, taskInfo, p.name, null), p.value, taskInfo.timestamp);
 				} else {
 					//(relatively) Silent failure, as all kinds of metrics can be sent here 
 					LOG.debug(String.format("Got datapoint with unsupported type, %s", p.value.getClass().getCanonicalName()));
@@ -99,5 +117,46 @@ public class GraphiteMetricsConsumer implements IMetricsConsumer {
 
 	public void cleanup() {
 		/* do nothing */
+	}
+	
+	/***
+	 * The graphite output path is determined by these constructs.
+	 * The metric value and the timestamp will always be appended to conform with graphite format
+	 * The default output string when metric key exists is: "%swh.%swp.%sti.%mn.%mk"
+	 * The default output string when metric key does not exist is: "%swh.%swp.%sti.%mn"
+	 * Both can be overriden via Storm configuration
+	 */
+	public enum OutputFormatConstructs {
+		SRC_COMPONENT_ID("%sci"),
+		SRC_TASK_ID("%sti"),
+		SRC_WORKER_HOST("%swh"),
+		SRC_WORKER_PORT("%swp"),
+		UPDATE_INTERVAL_SEC("%uis"),
+		METRIC_NAME("%mn"),
+		METRIC_KEY("%mk")
+		;
+		
+		String magicSequence;
+		
+		OutputFormatConstructs(String magicSequence) {
+			this.magicSequence = magicSequence;
+		}
+		
+		/***
+		 * Build the formatted string according to the required format
+		 * This is not the most efficient implementation, but this occurs only on metric registration, which is not very frequent
+		 */
+		public static String formatStringForGraphite(String formatString, TaskInfo taskInfo, String metricName, String metricKey) {
+			String ret = formatString.replace(SRC_COMPONENT_ID.magicSequence, taskInfo.srcComponentId);
+			ret = ret.replace(SRC_TASK_ID.magicSequence, String.valueOf(taskInfo.srcTaskId));
+			ret = ret.replace(SRC_WORKER_HOST.magicSequence, taskInfo.srcWorkerHost);
+			ret = ret.replace(SRC_WORKER_PORT.magicSequence, String.valueOf(taskInfo.srcWorkerPort));
+			ret = ret.replace(UPDATE_INTERVAL_SEC.magicSequence, String.valueOf(taskInfo.updateIntervalSecs));
+			ret = ret.replace(METRIC_NAME.magicSequence, metricName);
+			if(null != metricKey) {
+				ret = ret.replace(METRIC_KEY.magicSequence, metricKey);
+			}
+			return ret;
+		}
 	}
 }
